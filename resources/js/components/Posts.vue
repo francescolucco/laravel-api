b996f5f22f9cf9130e1368282508eef31771424a
francescolucco <francescolucco@googlemail.com>
2022-02-09 19:48:50 +0100

    indentazione

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,95 +1,86 @@
 <template>
-<main>
-   <div class="container">
-       <h1>I miei post</h1>
+    <main>
+        <div class="container">
+            <h1>I miei post</h1>
 
-       <PostItem
-       v-for="post in posts"
-       :key= "post.id"
-       :post= "post"
-       />
+            <PostItem v-for="post in posts" :key="post.id" :post="post" />
 
-       <div class="navigation">
-          <button
-          :disabled="pagination.current === 1"
-          @click="getPosts(pagination.current - 1)"
-          >Back</button>
-          <button
-          v-for="i in pagination.last"
-          :key="i"
-          @click="getPosts(i)"
-          :disabled="pagination.current === i"
-          >{{i}}</button>
-          <button
-          @click="getPosts(pagination.current + 1)"
-          >Next</button>
-       </div>
-   </div>
-</main>
+            <div class="navigation">
+                <button
+                    :disabled="pagination.current === 1"
+                    @click="getPosts(pagination.current - 1)"
+                >
+                    Back
+                </button>
+                <button
+                    v-for="i in pagination.last"
+                    :key="i"
+                    @click="getPosts(i)"
+                    :disabled="pagination.current === i"
+                >
+                    {{ i }}
+                </button>
+                <button @click="getPosts(pagination.current + 1)">Next</button>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
-
-import PostItem from './PostItem.vue'
-
+import PostItem from "./PostItem.vue";
 
 export default {
+    name: "Posts",
 
-   name: 'Posts',
+    components: {
+        PostItem,
+    },
 
-   components: {
-      PostItem
-   },
+    data() {
+        return {
+            apiUrl: "http://127.0.0.1:8000/api/posts?page=",
+            posts: null,
+            pagination: {},
+        };
+    },
 
-   data(){
-      return{
-         apiUrl: 'http://127.0.0.1:8000/api/posts?page=',
-         posts: null,
-         pagination:{},
-      }
-   },
-
-   mounted(){
-      this.getPosts();
-   },
-   methods:{
-      getPosts(page = 1){
-         axios.get(this.apiUrl + page)
-         .then(res => {
-            this.posts = res.data.data;
-            this.pagination = {
-               current: res.data.current_page,
-               last: res.data.last_page
-            };
-            console.log(this.pagination);
-         })
-      }
-   }
-}
-
+    mounted() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts(page = 1) {
+            axios.get(this.apiUrl + page).then((res) => {
+                this.posts = res.data.data;
+                this.pagination = {
+                    current: res.data.current_page,
+                    last: res.data.last_page,
+                };
+                console.log(this.pagination);
+            });
+        },
+    },
+};
 </script>
 
-<style lang='scss'>
-main{
-   padding: 150px 0;
-   .container{
-      h1{
-         margin-bottom: 30px;
-      }
-      .navigation{
-         button{
-            padding: 10px;
-            margin: 3px;
-            cursor: pointer;
-            border-radius: 10px;
-            &:hover{
-               background-color: black;
-               color: white;
+<style lang="scss">
+main {
+    padding: 150px 0;
+    .container {
+        h1 {
+            margin-bottom: 30px;
+        }
+        .navigation {
+            button {
+                padding: 10px;
+                margin: 3px;
+                cursor: pointer;
+                border-radius: 10px;
+                &:hover {
+                    background-color: black;
+                    color: white;
+                }
             }
-         }
-      }
-   }
-
+        }
+    }
 }
-
 </style>
