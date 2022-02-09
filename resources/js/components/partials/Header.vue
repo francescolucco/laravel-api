52c12ae0d1985f15082b28537874f27bae45b89d
francescolucco <francescolucco@googlemail.com>
2022-02-09 18:01:35 +0100

    header

--- resources/js/components/partials/Header.vue
+++ resources/js/components/partials/Header.vue
@@ -1,6 +1,21 @@
 <template>
   <header>
-     <nav>NAV</nav>
+     <nav>
+        <ul>
+           <li>
+              <a href="">Home</a>
+           </li>
+           <li>
+              <a class="active" href="">Blog</a>
+           </li>
+           <li>
+              <a href="">Chi Siamo</a>
+           </li>
+           <li>
+              <a href="">Contatti</a>
+           </li>
+        </ul>
+     </nav>
   </header>
 </template>
 
@@ -11,5 +26,28 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+
+header{
+   background-color: brown;
+   text-align: center;
+   padding: 20px 0;
+   ul{
+      list-style: none;
+      li{
+         display: inline-block;
+         margin: 0 15px;
+         a{
+            color: white;
+            text-decoration: none;
+            &:hover{
+               text-decoration: underline;
+            }
+            &.active{
+               color: yellow;
+            }
+         }
+      }
+   }
+}
 
 </style>
