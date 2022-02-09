901e32da05ae9be9e62e3fa0bf9c6dcd07e9bc27
francescolucco <francescolucco@googlemail.com>
2022-02-09 17:13:34 +0100

    aggiornamento rotte

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,55 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('posts', 'Api\PostController@index');
+
+
+
+// // 'prova' è il nome della rotta
+// // la function(){
+// //  ---
+// // }
+// // deve contenere dentro il return con il metodo response()->json(array)
+// // l'array verrà trasformato in un JSON vero e proprio da consegnare all'utente (chi ha chiamato la nostra API)
+
+// Route::get('prova', function(){
+
+//     return response()->json(
+//         [
+//             "name"=>"Ugo",
+//             "lastname"=>"De Ughi",
+//         ]
+//     );
+// });
+
+// // Se inizializzo una variable posso passrla direttamente
+
+// Route::get('prova', function(){
+
+//     $data =         [
+//         "name"=>"Ugo",
+//         "lastname"=>"De Ughi",
+//     ];
+
+//     return response()->json($data);
+// });
+
+// // Se mi serve più strutturato, con più dati, posso utilizzare compact
+
+// Route::get('prova', function(){
+
+//     $status =  'OK'   ;
+         
+//     $data =  [
+//         "name"=>"Ugo",
+//         "lastname"=>"De Ughi",
+//         "hobbies"=>[
+//             'golf',
+//             'curling',
+//             'bocce',
+//         ],
+//     ];
+
+//     return response()->json(compact('status', 'data'));
+// });
