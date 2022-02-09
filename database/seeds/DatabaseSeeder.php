cd104a0726a13f6331b3446150d0fa3989dbb34e
francescolucco <francescolucco@googlemail.com>
2022-02-09 14:55:25 +0100

    first-commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostsTableSeeder::class);
         $this->call(UpdatePostsSeeder::class);
         $this->call(TagsTableSeeder::class);
+        $this->call(PostsTagsTableSeeder::class);
     }
 }
